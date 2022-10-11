0496782b0bd7aae1950025d688ab0b7e1e37a583
JaafarHawli <jaafarhawle@gmail.com>
2022-10-11 23:55:13 +0300

    frontend: linked login form to backend

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from './api/axios';
 
 const Login = ({ setToken }) => {
 
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
+
+    const userLogin = async () => {
+        const form = {
+            email: username,
+            password: password
+        };
+        try {
+            const data = await axios.post('/api/v1/login', form);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
  
     return(
         <div className="login-wrapper">
@@ -20,7 +34,7 @@ const Login = ({ setToken }) => {
               <input type="password" onChange={e => setPassword(e.target.value)} />
             </label>
             <div>
-              <button type="submit">Submit</button>
+              <button type="button" onClick={userLogin}>Submit</button>
             </div>
           </form>
         </div>
