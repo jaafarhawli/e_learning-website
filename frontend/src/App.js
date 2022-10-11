51f95d04daa6ab2520d8e66b116dda74667d98ae
JaafarHawli <jaafarhawle@gmail.com>
2022-10-11 21:54:24 +0300

    frontend: added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import Panel from "./components/Panel";
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Instructors from './pages/Instructors';
-import Students from './pages/Students';
-import Courses from './pages/Courses';
-import Announcements from './pages/Announcements';
-
+import Admin from './AdminPage';
+import Login from './Login';
 
 
 function App() {
   return (
-    <div className="App flex">
-      <Router>
-        <Panel />
-        <Routes>
-          <Route path='/' exact element={<Dashboard />} />
-          <Route path='/instructors' element={<Instructors />} />
-          <Route path='/students' element={<Students />} />
-          <Route path='/courses' element={<Courses />} />
-          <Route path='/announcements' element={<Announcements />} />
-        </Routes>
-      </Router>
+    <div className="App">
+      {/* <Admin /> */}
+      <Login />
     </div>
   );
 }
---
+++ frontend/src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
+
+const Login = () => {
+  return (
+    <>
+            
+    </>
+  );
+}
+
+export default Login;
