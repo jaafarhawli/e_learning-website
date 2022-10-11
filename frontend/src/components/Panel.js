dbbcdda2f0bb6785d6cdc3535d8fe44fc0ac0f35
JaafarHawli <jaafarhawle@gmail.com>
2022-10-11 05:52:39 +0300

    frontend: added panel components to panel

--- frontend/src/components/Panel.js
+++ frontend/src/components/Panel.js
@@ -11,6 +11,12 @@ const Panel = () => {
   return (
     <div className="admin-panel flex column pointer">
         <h1 className="panel-component panel-header">Admin</h1>
+        <PanelComponent text='Jaafar Hawli' icon={<MdAdminPanelSettings className="panel-icon" />} />
+        <PanelComponent text='Instructors' icon={<FaUserTie className="panel-icon" />} />
+        <PanelComponent text='Students' icon={<FaUserGraduate className="panel-icon" />} />
+        <PanelComponent text='Courses' icon={<FaBook className="panel-icon" />} />
+        <PanelComponent text='Announcements' icon={<AiFillSound className="panel-icon" />} />
+        <PanelComponent text='Logout' icon={<FiLogOut className="panel-icon" />} />
     </div>
   );
 }
