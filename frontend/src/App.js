e68c57ae8c10953788d29270b8a4eaafc580335a
JaafarHawli <jaafarhawle@gmail.com>
2022-10-11 22:38:47 +0300

    frontend: added login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import Admin from './AdminPage';
 import Login from './Login';
 
@@ -6,8 +7,17 @@ import Login from './Login';
 function App() {
   return (
     <div className="App">
-      {/* <Admin /> */}
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/admin'>
+            <Admin />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
